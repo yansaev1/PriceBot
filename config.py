6622d98f754dee39a67ca5f61271eead8b086397
yansaev1 <126662576+yansaev1@users.noreply.github.com>
2023-05-24 19:36:38 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = '5749047384:AAG7I9shUPo3nbPiK5e5Sc_5bCe-8aBAZdo'
+
+keys = {
+    'Биткоин' : 'BTC',
+    'Эфириум' : 'ETH',
+    'Доллар' : 'USD',
+    'Евро' : 'EUR',
+    'Рубль' : 'RUB' }
